33b14b19017a2c87f34fadb91246c4965fab7a79
Sébastien David <sebastien-david@outlook.com>
2016-12-11 01:06:12 +0100

    fix prop type warning from tree-row

--- src/modules/tree/view/tree-row.js
+++ src/modules/tree/view/tree-row.js
@@ -11,7 +11,7 @@ class TreeRow extends PureComponent {
     children: ImmutablePropTypes.list,
     level: PropTypes.number.isRequired,
     path: ImmutablePropTypes.list.isRequired,
-    selected: PropTypes.bool.isRequired,
+    selected: PropTypes.bool,
     onSelect: PropTypes.func.isRequired,
   }
 
